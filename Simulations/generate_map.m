350e1b09eb1ae250f0a5ac9dff0464ee7e7ef659
Andreas Axen <axen@student.chalmers.se>
2023-08-11 22:51:50 +0200

    Map generation working, scaling working but something is still off

--- Simulations/generate_map.m
+++ Simulations/generate_map.m
@@ -1,15 +1,14 @@
-function complete_map = generate_map(nr_points, nr_spline_pts, map_offset, angle_cutoff, padding);
+function [map, min_x, min_y, max_x, max_y, start_x, start_y, start_angle] = generate_map(nr_points, nr_spline_pts, map_offset, angle_cutoff, padding, sensor_size);
 
 pts_list = zeros(nr_points,2);
 angle = zeros(nr_points-1,1);
 u = zeros(10,3);
 v = zeros(10,3);
 j = 0;
-mag_min = 1000;
-mag_max = 2000;
-map_size_x = 4000;
-map_size_y = 4000;
-sensor_size = 10;
+mag_min = 500;
+mag_max = 3000;
+map_size_x = 5000;
+map_size_y = 5000;
 
 for i=0:nr_points-2
     magnitude = randi([mag_min mag_max],1);
@@ -54,15 +53,15 @@ pts_list(end,2) = pts_list(1,2);
 t = cumsum([0;sqrt(sum(diff(pts_list,1,1).^2,2))]);
 ti = linspace(t(1),t(end),nr_spline_pts);
 xyzi = interp1(t,pts_list,ti,'spline');
-figure
+figure(1)
 plot(pts_list(:,1),pts_list(:,2),'or',xyzi(:,1),xyzi(:,2),'b');
 hold on
 plot(pts_list(:,1),pts_list(:,2))
 hold on
 plot(pts_list(1,1),pts_list(1,2),'*', 'LineWidth',2)
-axis equal
-title('Path');
-legend('','Path for map','Generated points');
+% axis equal
+% title('Path');
+% legend('','Path for map','Generated points');
 
 map = zeros(map_size_x, map_size_y);
 path = round(xyzi);
@@ -70,10 +69,25 @@ path = round(xyzi);
 for i=1:length(path)
     map(path(i,2) - sensor_size:path(i,2) + sensor_size, path(i,1) - sensor_size:path(i,1) + sensor_size) = 1;
 end
-min_path_x = min(path(:,1))-10;
-max_path_x = max(path(:,1))+10;
-min_path_y = min(path(:,2))-10;
-max_path_y = max(path(:,2))+10;
-complete_map = zeros((max_path_x - min_path_x),(max_path_y - min_path_y));
-complete_map = map(min_path_y:max_path_y,min_path_x:max_path_x);
-complete_map = padarray(complete_map,[padding, padding],0,'both');
+min_x = min(path(:,1))-sensor_size;
+max_x = max(path(:,1))+sensor_size;
+min_y = min(path(:,2))-sensor_size;
+max_y = max(path(:,2))+sensor_size;
+
+hold on
+plot([min_x,max_x],[max_y,max_y])
+plot([max_x,max_x],[max_y,min_y])
+plot([max_x,min_x],[min_y,min_y])
+plot([min_x,min_x],[min_y,max_y])
+hold off
+%start_y = min(xyzi(:,2));
+%start_x = round(xyzi((find(xyzi(:,2) == start_y)),1));
+%start_y = size(complete_map,2) - padding + sensor_size*1.5;
+start_x = path(1,1);;
+start_y =path(1,2);
+vector1 = [(path(50,1) - path(1,1)), (path(50,2) - path(1,2)), 0];
+vector2 = [1, 0, 0];
+start_angle = rad2deg(atan2(norm(cross(vector1,vector2)),dot(vector2,vector1)));
+
+
+
--- Simulations/startSim.m
+++ Simulations/startSim.m
@@ -1,27 +1,30 @@
 clc
 clear variables
 close all
-%%
-map = generate_map(9, 10000, 2000, 30, 500);
+nr_points = 9;                          % Number of points to generate the map
+nr_spline_pts = 10000;                  % How many spline points from those generated map points
+map_offset = 2500;                      % offset the map to be in possitive
+angle_cutoff = 20;                      % When to recalculate generated points to minimize sharp turns
+padding = 500;                          % Add space around the map
+sensor_size = 10;                       % Size of sensor in pixels
+pixel_conversion = 0.1;                 % Pixel to cm conversion
+pixel_sensor = 3;                       % 
+pixel_body = 10;
+v1 = 50;                                % Left  wheel velocity
+v2 = 50;                                % Right wheel velocity
 
-figure;
+spacing = 2/pixel_conversion;                 % Spacing between sensors 
+length_body = pixel_body/pixel_conversion;    % Distance between center and centor of sensors
+
+
+
+[map, min_x, min_y, max_x, max_y, offset_x, offset_y,start_angle] = generate_map(nr_points, nr_spline_pts, map_offset, angle_cutoff, padding, sensor_size);
+initialState = [0; 0; deg2rad(start_angle)];
 map = im2gray(map);
 map = im2bw(map);
 map = double(map);
 map = imcomplement(map);
-map = flipud(map);
-imshow(map)
-%%
-clc
-close all
-pixel = 2;
-offset_x = 80;                          %Offset for start position
-offset_y = 165;                         %Offset for start position
-v1 = 40;                                %Left  wheel velocity
-v2 = 40;                                %Right wheel velocity
-initialState = [0; 0; 0];
-spacing = 6;                            %Spacing between sensors 
-length_body = 15;                       %distance between center and centor of sensors
+%map = flipud(map);
 
 plots = 'sensor';   
 out = sim('simLink_line');
@@ -32,33 +35,34 @@ out2 = out.simout2.signals.values;  %sensor locations [x1, y1, x2, y2 x3, y3, x4
 out3 = out.simout3.signals.values;  %Sensor values [sensor1, sensor2, sensor3, sensor4, sensor5, sensor6, sensor7]
 out4 = out.simout4.signals.values;  %Speed after control [Speed]
 
-sensor_x = 1;
-sensor_y = 2;
+sensor_x = 9;
+sensor_y = 10;
 %figure;
-%map_sensor = map(out2(1,sensor_y) + offset_y - pixel:out2(1,sensor_y) + offset_y + pixel, out2(1,sensor_x) + offset_x - pixel:out2(1,sensor_x) + offset_x + pixel);
+%map_sensor = map(out2(1,sensor_y) + offset_y - pixel_sensor:out2(1,sensor_y) + offset_y + pixel_sensor, out2(1,sensor_x) + offset_x - pixel_sensor:out2(1,sensor_x) + offset_x + pixel_sensor);
 %imshow(map_sensor);
 
 
 if(strcmp(plots,'sensor')||strcmp(plots,'all'))
     figure;
     RI = imref2d(size(map));
-    RI.XWorldLimits = [0 length(map(:,1))];
-    RI.YWorldLimits = [0 length(map(:,2))];
+    RI.XWorldLimits = [0 size(map,1)];
+    RI.YWorldLimits = [0 size(map,2)];
     for i=1:length(out0)
-        imshow(imcomplement(map),RI);
+        imshow(map,RI);
         set(gca,'YDir','normal')
         hold on
         plot(out0(i,1) + offset_x,out0(i,2) + offset_y,'LineWidth',1.5);
-        plot(out2(i,1) + offset_x,out2(i,2) + offset_y,'b*');
-        plot(out2(i,3) + offset_x,out2(i,4) + offset_y,'b*');
-        plot(out2(i,5) + offset_x,out2(i,6) + offset_y,'b*');
-        plot(out2(i,7) + offset_x,out2(i,8) + offset_y,'b*');
-        plot(out2(i,9) + offset_x,out2(i,10) + offset_y,'b*');
-        plot(out2(i,11) + offset_x,out2(i,12) + offset_y,'b*');
-        plot(out2(i,13) + offset_x,out2(i,14) + offset_y,'b*');
+        plot(out2(i,1) + offset_x,out2(i,2) + offset_y,'*');
+        plot(out2(i,3) + offset_x,out2(i,4) + offset_y,'*');
+        plot(out2(i,5) + offset_x,out2(i,6) + offset_y,'*');
+        plot(out2(i,7) + offset_x,out2(i,8) + offset_y,'*');
+        plot(out2(i,9) + offset_x,out2(i,10) + offset_y,'*');
+        plot(out2(i,11) + offset_x,out2(i,12) + offset_y,'*');
+        plot(out2(i,13) + offset_x,out2(i,14) + offset_y,'*');
         plot(out0(i,1) + offset_x,out0(i,2) + offset_y,'o','MarkerFaceColor',[1,0,0]);
         axis equal
         hold off
+        legend('','Sensor 1','Sensor 2','Sensor 3','Sensor 4','Sensor 5','Sensor 6','Sensor 7')
         pause(0.001);
     end
 end
