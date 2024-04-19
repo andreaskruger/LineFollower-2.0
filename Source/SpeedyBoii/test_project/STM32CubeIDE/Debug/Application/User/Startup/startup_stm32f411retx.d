2dd42b8057f56fc66349bb6d5aef33c646bfa950
Dquach <daniel.quach@aros.se>
2024-04-19 16:35:55 +0200

    add speedyboii cubeide project

---
+++ Source/SpeedyBoii/test_project/STM32CubeIDE/Debug/Application/User/Startup/startup_stm32f411retx.d
@@ -0,0 +1,2 @@
+Application/User/Startup/startup_stm32f411retx.o: \
+ ../Application/User/Startup/startup_stm32f411retx.s
---
+++ Source/SpeedyBoii/test_project/STM32CubeIDE/Debug/Drivers/STM32F4xx_HAL_Driver/stm32f4xx_ll_adc.d
@@ -0,0 +1,2 @@
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_ll_adc.o: \
+ C:/Users/DQUACH/Documents/Test_LineSTM32/test_project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c
